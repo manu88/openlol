4e50d3729fb0d3896893be1b68a63b46c5f85899
manuel deneu <manuel.deneu@gmail.com>
2025-08-28 09:14:38 +0200

    add CLI switch

--- src/main.c
+++ src/main.c
@@ -7,67 +7,60 @@
 #include <stdlib.h>
 #include <string.h>
 
-extern uint8_t *paletteBuffer;
+static void usagePak(void) { printf("pak subcommands: list\n"); }
 
-int main(int argc, char *argv[]) {
+static int cmdPakList(const char *pakfilepath) {
+  printf("List for '%s'\n", pakfilepath);
   PAKFile file;
   PAKFileInit(&file);
 
-  if (PAKFileRead(
-          &file,
-          "/Users/manueldeneu/Downloads/setup-00399-Lands_of_Lore-PCLinux20/"
-          "sources/C/LandsOL/INTRO.PAK") == 0) {
+  if (PAKFileRead(&file, pakfilepath) == 0) {
     perror("Error while reading file");
     return 1;
   }
   printf("read %i entries\n", file.count);
 
-  uint8_t *imgBuffer = NULL;
   for (int i = 0; i <= file.count; i++) {
     PAKEntry *entry = &file.entries[i];
     printf("%i: Entry offset %u name '%s' ('%s') size %u \n", i, entry->offset,
            entry->filename, PakFileEntryGetExtension(entry), entry->fileSize);
-    if (strcmp(entry->filename, "BACKGRND.CPS") == 0) {
-      const uint8_t *buffer = PakFileGetEntryData(&file, entry);
-      imgBuffer = TestCps(buffer, entry->fileSize);
-      break;
-    }
   }
-  if (imgBuffer) {
 
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Event event;
-    SDL_Renderer *renderer;
-    SDL_Window *window;
-    SDL_CreateWindowAndRenderer(320, 200, 0, &window, &renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    SDL_RenderClear(renderer);
-
-    int maxPixVal = 0;
-    for (int i = 0; i < 64000; i++) {
-      uint8_t pix = imgBuffer[i];
-      if (pix > maxPixVal) {
-        maxPixVal = pix;
-      }
-      uint8_t r = (paletteBuffer[(pix * 3)] * 255) / 63;
-      uint8_t g = (paletteBuffer[(pix * 3) + 1] * 255) / 63;
-      uint8_t b = (paletteBuffer[(pix * 3) + 2] * 255) / 63;
-
-      int x = i % 320;
-      int y = i / 200;
-      SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-      SDL_RenderDrawPoint(renderer, x, y);
-    }
-
-    SDL_RenderPresent(renderer);
-    while (1) {
-      if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-        break;
-    }
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-  }
   PAKFileRelease(&file);
   return 0;
+}
+
+static int cmdPak(int argc, char *argv[]) {
+  if (argc < 1) {
+    printf("pak command, missing arguments\n");
+    usagePak();
+    return 1;
+  }
+  if (strcmp(argv[0], "list") == 0) {
+    if (argc < 2) {
+      printf("pak list: missing pak file path\n");
+      return 1;
+    }
+    return cmdPakList(argv[1]);
+  }
+  printf("Unknown pak command '%s'\n", argv[0]);
+  usagePak();
+  return 1;
+}
+
+static void usage(const char *progName) {
+  printf("%s: pak subcommand ...\n", progName);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    usage(argv[0]);
+    return 0;
+  }
+  if (strcmp(argv[1], "pak") == 0) {
+    return cmdPak(argc - 2, argv + 2);
+  }
+  printf("Unknown command '%s'\n", argv[1]);
+  usage(argv[0]);
+  return 1;
 }
