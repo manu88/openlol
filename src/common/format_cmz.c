c5f4f7a8e43ff99a70d838442aac77c993ab1723
manuel deneu <manuel.deneu@gmail.com>
2025-09-23 07:48:00 +0200

    remove unused code

--- src/common/format_cmz.c
+++ src/common/format_cmz.c
@@ -10,77 +10,10 @@
 #include <string.h>
 
 #define GETWORD(x) ((x)[0] | ((x)[1] << 8))
-#define GETLONG(x) ((x)[0] | ((x)[1] << 8) | ((x)[2] << 16) | ((x)[3] << 24))
 #define MAXSRCLEN (64 * 1024)    /* max length of crunched data */
 #define MAXDESTLEN (1024 * 1024) /* max length of unpacked data */
 
-#define EMPTYFILE (0)
 #define OVERFLOW (-1)
-#define UNKNOWNCOMP (-2)
-
-/* compression mode 0 = no compression */
-static int cps_copy(unsigned char *src, unsigned char *dest) {
-  int len, x;
-
-  /* get the length of the 'uncompressed' data */
-  if ((len = GETLONG(src)) == 0)
-    return EMPTYFILE;
-
-  /* check for source overflow. we can't overflow the destination */
-  if (len < 0 || len > (MAXSRCLEN - GETWORD(&src[4]) - 6))
-    return OVERFLOW;
-
-  /* skip to the start of data and do a direct copy from source to dest */
-  src += GETWORD(&src[4]) + 6;
-  for (x = len; x--;)
-    *dest++ = *src++;
-
-  return len;
-}
-
-/* compression mode 3 = run-length encoding */
-static int cps_rle(unsigned char *src, unsigned char *dest) {
-  int len, x;
-  char rep;
-
-  /* get the length of the 'uncompressed' data */
-  if ((len = GETLONG(src)) == 0)
-    return EMPTYFILE;
-
-  /* check for destination overflow. */
-  if (len < 0 || len > MAXDESTLEN)
-    return OVERFLOW;
-
-  /* skip to start of data */
-  src += GETWORD(&src[4]) + 6;
-
-  /* run while we have room left in output buffer */
-  for (x = len; x > 0;) {
-    int rlen = *src++;
-    if (rlen < 128) {
-      /* direct copy, no RLE */
-      x -= rlen;
-      if (x < 0)
-        return OVERFLOW;
-      while (rlen--)
-        *dest++ = *src++;
-    } else {
-      /* repeated character RLE */
-      if (rlen == 0) {
-        rlen = GETWORD(src);
-        src += 2;
-      } else
-        rlen = 256 - rlen;
-      x -= rlen;
-      if (x < 0)
-        return OVERFLOW;
-      rep = *src++;
-      while (rlen--)
-        *dest++ = rep;
-    }
-  }
-  return len - x;
-}
 
 /* compression mode 4 = LZ77 style compression */
 static int cps_lz77(const unsigned char *src, unsigned char *dest) {
