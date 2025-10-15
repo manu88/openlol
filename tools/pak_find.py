08c54b67f78997b5de35685ec80a953540d91c37
manuel deneu <manuel.deneu@gmail.com>
2025-10-15 14:48:39 +0200

    tools: add -extract option to pak_find

--- tools/pak_find.py
+++ tools/pak_find.py
@@ -2,23 +2,38 @@ from os import listdir
 from os.path import isfile, join
 import subprocess
 import sys
+import argparse
 
 pak_path = "data/cd"
 pak_files = [join(pak_path, f) for f in listdir(pak_path) if isfile(
     join(pak_path, f)) and f.endswith(".PAK")]
 # print(pak_files)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
+parser.add_argument("-e", action="store_true", help="extract file")
+
+
+def extract(file: str, pak: str):
+    argv = ["./lol", "-p", f"{pak}", "pak", "extract", file]
+    proc_output = subprocess.run(
+        argv, stdout=subprocess.PIPE).stdout.decode()
+    print(f"extracted {file}")
+
 
 def main():
-    file_to_find = sys.argv[1].upper()
+    args = parser.parse_args()
+    file_to_find = args.file
     print(f"Looking for {file_to_find} in {pak_path}")
     for pak_file in pak_files:
-        args = ["./lol", "-p", f"{pak_file}", "pak", "list"]
+        argv = ["./lol", "-p", f"{pak_file}", "pak", "list"]
         proc_output = subprocess.run(
-            args, stdout=subprocess.PIPE).stdout.decode()
+            argv, stdout=subprocess.PIPE).stdout.decode()
         for l in proc_output.splitlines():
             if l == file_to_find:
                 print(f"Found {file_to_find} in {pak_file}")
+                if (args.e):
+                    extract(file_to_find, pak_file)
                 return
     print("No such file")
 
