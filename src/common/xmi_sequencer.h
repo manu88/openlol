dcf61eae754f9d386626826e40104e2a824fde29
manuel deneu <manuel.deneu@gmail.com>
2026-02-13 16:41:39 +0100

    xmi: refactoring

--- src/common/xmi_sequencer.h
+++ src/common/xmi_sequencer.h
@@ -1,12 +1,18 @@
 #pragma once
 
 #include "formats/format_xmi.h"
+#include <stddef.h>
 #include <stdint.h>
 
 typedef struct {
   uint8_t done;
   double ticksPerSec;
+
+  const XMISequence *currentSeq;
+  uint8_t *head;
+
 } XMISequencer;
 
 void XMISequencerInit(XMISequencer *seq);
+void XMISequencerReset(XMISequencer *seq);
 void XMISequencerPlay(XMISequencer *seq, const XMISequence *sequence);
